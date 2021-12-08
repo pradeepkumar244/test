432c60228292618d18aca503598d77616af40876
pradeepkumar244 <pradeepkumar24496@gmail.com>
2021-12-08 19:31:24 +0530

    deploy testin github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import pro from './img/ppp.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>pradeepkumar</h1>
+     <h1>coding test</h1>
+     <img src={pro} alt="proimg"/>
+    </div>
+  );
+}
+
+export default App;
